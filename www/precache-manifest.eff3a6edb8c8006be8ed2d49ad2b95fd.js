607edab5f3589c482aeaf393edf313f0f1e63ef2
DRAJ <davidrajnus@gmail.com>
2019-08-19 22:46:08 +0800

    first init

---
+++ www/precache-manifest.eff3a6edb8c8006be8ed2d49ad2b95fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796df6a7b69928ddb16a324fa9c5345e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57ac99f849e57eaebe68",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4c54a5253c3240e915c8",
+    "url": "./static/js/2.27b04e90.chunk.js"
+  },
+  {
+    "revision": "57ac99f849e57eaebe68",
+    "url": "./static/js/main.45dc83ba.chunk.js"
+  },
+  {
+    "revision": "040d09c0c5e45d6fc66b",
+    "url": "./static/js/runtime~main.c210ed45.js"
+  },
+  {
+    "revision": "724e499ae8664b229be0f1ac01286143",
+    "url": "./static/media/image.724e499a.png"
+  }
+]);
